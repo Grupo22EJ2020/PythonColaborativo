805574c5db9618030f51af1b10bcd73c34f43cf5
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2020-05-06 16:58:38 -0500

    Commit inicial

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Bienvenido al programa")
+print("Personas de la organizacion")
+print("1.-Pasatiempos de Liigabriel")
+opcion = int(input("Que opcion eliges"))
+if opcion == 1:
+    print("Eligio el miembro 1")
