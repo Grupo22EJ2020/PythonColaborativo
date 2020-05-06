ea90943ed8abe495f8689c622651e5a507fb09e1
aldo1871 <aaldoo10@hotmail.com>
2020-05-06 17:40:28 -0500

    Agregue usuario 5

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ print("1.-Pasatiempos de Liigabriel")
 print("2.-Pasatiempos de bryan20nietosi")
 print("3.-Pasatiempos otro alumno (lo ayude)")
 print("4.-Pasatiempos de Orlando190")
+print("5.-Pasatiempos de aldo1871")
 opcion = int(input("Que opcion eliges"))
 if opcion == 1:
     print("Eligio el miembro 1")
@@ -13,3 +14,5 @@ elif opcion ==3:
     print("Eligio otro alumno")
 elif opcion==4:
     print("Eligio el miembro 4")
+elif opcion==5:
+    print("Eligio el miembro 5")
