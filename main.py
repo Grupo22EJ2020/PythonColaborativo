cad60e26904dd4293753a13b1fdceb6cdcd28105
Orlando Mendoza Campos <orlando.mendozacm@uanl.edu.mx>
2020-05-06 17:26:23 -0500

    Se agrego miembro 4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print("Personas de la organizacion")
 print("1.-Pasatiempos de Liigabriel")
 print("2.-Pasatiempos de bryan20nietosi")
 print("3.-Pasatiempos otro alumno (lo ayude)")
+print("4.-Pasatiempos de Orlando190")
 opcion = int(input("Que opcion eliges"))
 if opcion == 1:
     print("Eligio el miembro 1")
@@ -10,3 +11,5 @@ elif opcion==2:
     print("Eligio el miembro 2")
 elif opcion ==3:
     print("Eligio otro alumno")
+elif opcion==4:
+    print("Eligio el miembro 4")
