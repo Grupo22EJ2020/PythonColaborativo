e23fbf5c9ef27b5e965036afcaaa239637f622b7
Cesar Lopez <zepol14117@gmail.com>
2020-05-06 17:36:37 -0500

    se agrego esto

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 print("Bienvenido al programa")
 print("Personas de la organizacion")
 print("1.-Pasatiempos de Liigabriel")
+print("6.-Pasatiempos ArcelLopez11")
 opcion = int(input("Que opcion eliges"))
 if opcion == 1:
     print("Eligio el miembro 1")
+elif opcion == 4:
+    print("Eligo el miebro 6")
