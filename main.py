cfdeab2bc182b19467f46b6f4814206a8ce3d493
Daniel Eduardo Rojas Garza <danymessi1000@gmail.com>
2020-05-06 18:06:47 -0500

    Agregue miembro 7

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ print("3.-Pasatiempos otro alumno (lo ayude)")
 print("4.-Pasatiempos de Orlando190")
 print("5.-Pasatiempos de aldo1871")
 print("6.-Pasatiempos de ArcelLopez11")
+print("7-Pasatiempos de DanielRojas2002")
 opcion = int(input("Que opcion eliges"))
 if opcion == 1:
     print("Eligio el miembro 1")
@@ -19,4 +20,6 @@ elif opcion ==5:
     print("Eligo el miembro 5")
 elif opcion==6:
     print("Eligio el miembro 6")
+elif opcion==7:
+    print("Eligio el miembro 7")
 
