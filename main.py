1e781b11493597131f0d64572b732256c428b258
Cesar Lopez <zepol14117@gmail.com>
2020-05-06 17:44:54 -0500

    Agregue mi usuario

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 print("Bienvenido al programa")
 print("Personas de la organizacion")
 print("1.-Pasatiempos de Liigabriel")
-<<<<<<< HEAD
-print("6.-Pasatiempos ArcelLopez11")
-opcion = int(input("Que opcion eliges"))
-if opcion == 1:
-    print("Eligio el miembro 1")
-elif opcion == 4:
-    print("Eligo el miebro 6")
-=======
 print("2.-Pasatiempos de bryan20nietosi")
 print("3.-Pasatiempos otro alumno (lo ayude)")
 print("4.-Pasatiempos de Orlando190")
+print("5.-Pasatiempos de ArcelLopez11")
 opcion = int(input("Que opcion eliges"))
 if opcion == 1:
     print("Eligio el miembro 1")
@@ -21,4 +14,5 @@ elif opcion ==3:
     print("Eligio otro alumno")
 elif opcion==4:
     print("Eligio el miembro 4")
->>>>>>> cad60e26904dd4293753a13b1fdceb6cdcd28105
+elif opcion ==5:
+    print("Eligo el miembro 5")
