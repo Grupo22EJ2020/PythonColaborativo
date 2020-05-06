040620773d18e108989c0f6f3edf859d073acc8f
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2020-05-06 17:15:15 -0500

    Agregue a un alumno que solicito ayuda

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 print("Bienvenido al programa")
 print("Personas de la organizacion")
 print("1.-Pasatiempos de Liigabriel")
+print("2.-Pasatiempos otro alumno (lo ayude)")
 opcion = int(input("Que opcion eliges"))
 if opcion == 1:
     print("Eligio el miembro 1")
+elif opcion ==2:
+    print("Eligio otro alumno")
