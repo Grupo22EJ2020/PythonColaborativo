5c17f207eaff494a6b50db9535d94dab4235dbbc
Bryan Nieto Silva <bryan.nietosi@uanl.edu.mx>
2020-05-06 17:06:27 -0500

    cambio

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 print("Bienvenido al programa")
 print("Personas de la organizacion")
 print("1.-Pasatiempos de Liigabriel")
+print("2.-Pasatiempos de bryan20nietosi")
 opcion = int(input("Que opcion eliges"))
 if opcion == 1:
     print("Eligio el miembro 1")
+elif opcion==2:
+    print("Eligio el miembro 2")
