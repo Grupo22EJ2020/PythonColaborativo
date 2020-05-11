6ecaec3ead003f6c3d170c9d0e2b4c8ab42eeb7e
Carlos Alan Guerrero Belmonte <jupiterguerrero92@gmail.com>
2020-05-11 18:00:57 -0500

    se agrego la clase appolion44

---
+++ appolion44.py
@@ -0,0 +1,5 @@
+class Appolion44: 
+    def mostrarPasatiempos(self): 
+        print("mi principal masatiempo es escuchar musica")
+        print ("mi segundo pasatiempo es ver peliculas ")
+        print("otro de mis pasatiempos es jugar videojuegos") 
