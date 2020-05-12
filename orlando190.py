4ab54f1f5e195055149cbfcb05daca59f89cda74
Orlando Mendoza Campos <orlando.mendozacm@uanl.edu.mx>
2020-05-11 21:53:59 -0500

    add clase orlando190.py

---
+++ orlando190.py
@@ -0,0 +1,6 @@
+class Orlando190:
+    def imprimirPasatiempos(self):
+        print("Mis pasatiempos son:")
+        print("Me gusta ver videos en youtube")
+        print("Me gusta jugar y ver el futbol")
+        print("Jugar Videojuegos")
