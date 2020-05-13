eece417f43a7af9854827151e0441de9f6c40e99
yaressildiaz1101 <alin.lopezdz@uanl.edu.mx>
2020-05-13 02:08:44 -0500

    Add clase Yaressi

---
+++ yaressildiaz1101.py
@@ -0,0 +1,5 @@
+class Yaressildiaz1101:
+    def imprimirPasatiempos(self):
+        print ("Me gusta maquillarme")
+        print ("Me gusta salir de fiesta")
+        print ("Me gusta ir a la iglesia")
