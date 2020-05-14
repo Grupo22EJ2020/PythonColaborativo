8e50aa4ea766589a8ea8fcfc2ea46e1fd8647c15
LilianaGR <lizethreyes841@gmail.com>
2020-05-13 20:00:05 -0500

    agregue mi archivo

---
+++ lilianagr.py
@@ -0,0 +1,7 @@
+class Lilianagr:
+    def imprimirPasatiempos(self):
+        print("Mis pasatiempos son:")
+        print("Me gusta jugar con mis gatos")
+        print("Me gusta leer libros")
+        print("Me gusta leer poemas")
+
