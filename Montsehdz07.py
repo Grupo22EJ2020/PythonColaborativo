438979b0c3742ef87aa393bda10d1a8c00509ecf
Montsehdz07 <montsee.hernandez07@gmail.com>
2020-05-16 16:35:35 -0500

    agregue mis pasatiempos

---
+++ Montsehdz07.py
@@ -0,0 +1,6 @@
+class montsehdz07: 
+    def InfoPasatiempos(self):
+        print("Mis pasatiempos favoritos son:")
+        print("Jugar softball")
+        print("ver series")
+        print("pintar")
