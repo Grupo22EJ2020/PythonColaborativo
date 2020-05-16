0e13830954a8a73d73704295bc3956d7871eae15
ibarrajuan10 <juan.ibarra.mty@outlook.com>
2020-05-16 16:29:54 -0500

    Agregue mi clase

---
+++ ibarrajuan10.py
@@ -0,0 +1,7 @@
+class IbarraJuan10:
+    def PasatiemposFavoritos(self):
+        print("Mis pasatiempos son: ")
+        print("Jugar futbol")
+        print("Ir a fiestas con mis amigos")
+        print("Jugar PS4")
+        print("Ver peliculas")
