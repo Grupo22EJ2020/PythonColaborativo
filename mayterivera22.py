7088d1ad90aa1751e4c67b99b54ce9256c4a68da
María Teresa Palomo Rivera <mayterivera22@outlook.es>
2020-05-20 18:09:53 -0500

    volvi a agregar clase

---
+++ mayterivera22.py
@@ -0,0 +1,6 @@
+class MayteRivera22:
+    def imprimirPasatiempos(self):
+        print("Mis pasatiempos son:")
+        print("Me gusta entrenar")
+        print("Me gusta ver peliculas")
+        print("Me gusta hacer postres")
