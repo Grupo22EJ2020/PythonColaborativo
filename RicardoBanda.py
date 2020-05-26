bcfd78a7a619455ec73f640686727b6926549bcd
RicardoBanda <alexjeje453@gmail.com>
2020-05-25 22:45:11 -0500

    Ahora si agregue pasa tiempos

---
+++ RicardoBanda.py
@@ -0,0 +1,7 @@
+class RicardoBanda:
+    def mispasatiempos(self):
+        print("Mis pasatiempos son:")
+        print("Jugar Vide -sojuegos")
+        print("Ver series de superheroes")
+        print("Hacer algun deporte")
+        print("Leer comics")
