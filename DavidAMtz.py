32e2c5cd0cf801a0321fd4e050eaf0ab053512a6
David Adrian Martinez Saldaña <davidadrian272019@hotmail.com>
2020-05-27 10:07:16 -0500

    Agregue mi clase de mis pasatiempos

---
+++ DavidAMtz.py
@@ -0,0 +1,7 @@
+class DavidAMtz:
+    def ImprimirPasatiempos(self):
+        print("Mis pasatiempos son: ")
+        print("Escuchar música pop")
+        print("ir de compras")
+        print("Ver series y peliculas en Netflix")
+        
