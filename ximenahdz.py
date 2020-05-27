cded5fab851aa4bac6b5671db04c2494a8ca967e
Ximena Martitínez Hernández <ximenamthdz@gmail.com>
2020-05-27 16:55:35 -0500

    Agregué mis pasatiempos

---
+++ ximenahdz.py
@@ -0,0 +1,7 @@
+class ximenahdz:
+    def imprimirPasatiempos(self):
+        print ("Mis pasatiempos son: ")
+        print ("Grabar y subir tiktoks")
+        print ("Me gusta cantar")
+        print ("Me gusta ver series y peliculas")
+
