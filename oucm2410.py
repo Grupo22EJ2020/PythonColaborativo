c3a4d1229cb29fbb3ab1768522cf4ffe3ffef596
Oscar Ulises Caballero Mata <cmu33723@gmail.com>
2020-05-27 23:38:46 -0500

    Agregue mis pasatiempos

---
+++ oucm2410.py
@@ -0,0 +1,6 @@
+class oucm2410:
+    def imprimirPasatiempos(self):
+        print("Mis pasatiempos son:")
+        print("Ver Netflix")
+        print("Ver youtubers")
+        print("Salir con amigos")
